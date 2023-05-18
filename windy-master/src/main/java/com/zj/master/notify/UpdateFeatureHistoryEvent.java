847099b84dc5c4cf61b35c33732057221790e80a
falcon <falcon.hu@tuya.com>
2023-05-18 11:01:21 +0800

    feat: 完善用例执行流程代码

--- windy-master/src/main/java/com/zj/master/notify/UpdateFeatureHistoryEvent.java
+++ windy-master/src/main/java/com/zj/master/notify/UpdateFeatureHistoryEvent.java
@@ -3,7 +3,9 @@ package com.zj.master.notify;
 import com.alibaba.fastjson.JSON;
 import com.zj.common.enums.NotifyType;
 import com.zj.common.model.ResultEvent;
+import com.zj.domain.entity.dto.feature.FeatureHistoryDto;
 import com.zj.domain.repository.feature.IFeatureHistoryRepository;
+import com.zj.master.dispatch.feature.FeatureExecuteProxy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,6 +21,9 @@ public class UpdateFeatureHistoryEvent implements INotifyEvent {
   @Autowired
   private IFeatureHistoryRepository featureHistoryRepository;
 
+  @Autowired
+  private FeatureExecuteProxy featureExecuteProxy;
+
   @Override
   public NotifyType type() {
     return NotifyType.UPDATE_FEATURE_HISTORY;
@@ -28,7 +33,12 @@ public class UpdateFeatureHistoryEvent implements INotifyEvent {
   public boolean handle(ResultEvent resultEvent) {
     log.info("receive node record create event id = {} event={}", resultEvent.getExecuteId(),
         JSON.toJSONString(resultEvent.getParams()));
-    return featureHistoryRepository.updateStatus(resultEvent.getExecuteId(),
+    FeatureHistoryDto featureHistoryDto = JSON.parseObject(
+        JSON.toJSONString(resultEvent.getParams()), FeatureHistoryDto.class);
+    boolean updateStatus = featureHistoryRepository.updateStatus(resultEvent.getExecuteId(),
         resultEvent.getStatus().getType());
+
+    featureExecuteProxy.featureStatusChange(resultEvent.getExecuteId(), featureHistoryDto);
+    return updateStatus;
   }
 }
