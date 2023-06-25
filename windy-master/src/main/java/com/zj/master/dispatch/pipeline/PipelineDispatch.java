406d2622b3726a2da25e155e1e32fbb7af0015e5
guyuelan <1561107002@qq.com>
2023-06-25 19:06:24 +0800

    fix: 分配任务添加pipelineId

--- windy-master/src/main/java/com/zj/master/dispatch/pipeline/PipelineDispatch.java
+++ windy-master/src/main/java/com/zj/master/dispatch/pipeline/PipelineDispatch.java
@@ -167,6 +167,7 @@ public class PipelineDispatch implements IDispatchExecutor {
 
     ActionDetail actionDetail = new ActionDetail(configDetail, action);
     RequestContext requestContext = RequestContextBuilder.createContext(actionDetail);
+    requestContext.setPipelineId(pipelineNode.getPipelineId());
     taskNode.setRequestContext(requestContext);
 
     RefreshContext refreshContext = RefreshContextBuilder.createContext(actionDetail);
