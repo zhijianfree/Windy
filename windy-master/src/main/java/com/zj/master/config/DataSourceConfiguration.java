4a6a68d27923310545c0df3c40255d7f04eb4b24
guyuelan <1561107002@qq.com>
2023-08-17 21:32:09 +0800

    简化配置，加载前端资源
    * 修复bug
    
    * 删除多余配置

---
+++ windy-master/src/main/java/com/zj/master/config/DataSourceConfiguration.java
@@ -0,0 +1,31 @@
+package com.zj.master.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+    public static final String MARIADB_JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+    @Value("${DB_URL}")
+    private String dbUrl;
+
+    @Value("${DB_USERNAME}")
+    private String dbUsername;
+
+    @Value("${DB_PASSWORD}")
+    private String dbPassword;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(MARIADB_JDBC_DRIVER);
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+    }
+}
--- windy-starter/src/main/java/com/zj/starter/WindyApplication.java
+++ windy-starter/src/main/java/com/zj/starter/WindyApplication.java
@@ -3,7 +3,7 @@ package com.zj.starter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.zj")
 @MapperScan("com.zj.*.mapper")
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
 public class WindyApplication {
 
   public static void main(String[] args) {
