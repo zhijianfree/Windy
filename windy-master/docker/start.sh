721d37ad9ed4092aed1ee8ab29e10b638f3e57d0
guyuelan <1561107002@qq.com>
2024-10-14 11:21:18 +0800

    调整Dockerfile

--- windy-master/docker/start.sh
+++ windy-master/docker/start.sh
@@ -1,41 +1,13 @@
 #!/bin/bash
 
 service="Windy"
-JAR_NAME="*.jar"
 
 SYSTEM_CACHE_SIZE=$(awk '($1 == "MemTotal:"){print $2}' /proc/meminfo)
 echo "$SYSTEM_CACHE_SIZE"
 
-JVM_CACHE_SIZE=256
-NEW_CACHE_SIZE=128
-
-if [ $SYSTEM_CACHE_SIZE -gt 524288 ] ; then
-  JVM_CACHE_SIZE=512
-  NEW_CACHE_SIZE=256
-fi
-
-if [  $SYSTEM_CACHE_SIZE -gt 1048576 ] ; then
-  JVM_CACHE_SIZE=1024
-  NEW_CACHE_SIZE=512
-fi
-
-if [  $SYSTEM_CACHE_SIZE -gt 2097152 ] ; then
-  JVM_CACHE_SIZE=2048
-  NEW_CACHE_SIZE=1024
-fi
-
-if [ $SYSTEM_CACHE_SIZE -gt 3151872 ] ; then
-  JVM_CACHE_SIZE=3078
-  NEW_CACHE_SIZE=1536
-fi
-
-JVM_OPTION="-Xms${JVM_CACHE_SIZE}m -Xmx${JVM_CACHE_SIZE}m -XX:MaxNewSize=${NEW_CACHE_SIZE}m -XX:SurvivorRatio=6"
-
 echo "start service $service"
-echo "$JVM_OPTION"
 
-nohup java -jar $JVM_OPTION $JAR_NAME
-
+nohup java -jar *.jar
 
 sleep 10
 
