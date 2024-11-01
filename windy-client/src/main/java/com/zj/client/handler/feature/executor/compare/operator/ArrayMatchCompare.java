e6e45c28400cb0cf59c157908d83aa76a6ddd0b4
guyuelan <1561107002@qq.com>
2024-11-01 14:40:20 +0800

    优化比较逻辑

--- windy-client/src/main/java/com/zj/client/handler/feature/executor/compare/operator/ArrayMatchCompare.java
+++ windy-client/src/main/java/com/zj/client/handler/feature/executor/compare/operator/ArrayMatchCompare.java
@@ -6,17 +6,21 @@ import com.zj.client.handler.feature.executor.compare.ognl.OgnlDataParser;
 import com.zj.common.enums.CompareType;
 import com.zj.common.exception.ErrorCode;
 import com.zj.common.feature.CompareDefine;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 @Component
 public class ArrayMatchCompare extends BaseCompare{
 
-    public static final String SELF = "self";
+    private final Pattern pattern = Pattern.compile("\\{(.*?)\\}(.*)");
     private final OgnlDataParser ognlDataParser = new OgnlDataParser();
     @Override
     public CompareType getType() {
@@ -27,21 +31,20 @@ public class ArrayMatchCompare extends BaseCompare{
     public CompareResult compare(CompareDefine compareDefine) {
         CompareResult compareResult = createSuccessResult();
         try {
-            List<Object> responseList = (List<Object>)compareDefine.getResponseValue();
-            String compareKey = compareDefine.getCompareKey();
-            String[] strings = compareKey.split("\\.");
-            String propertyName = strings[strings.length - 1];
-            log.info("compare key = {} response value={}", propertyName, JSON.toJSONString(compareDefine.getExpectValue()));
+            List<Object> responseList = JSON.parseArray(JSON.toJSONString(compareDefine.getResponseValue()), Object.class);
+            PattenEntry pattenEntry = convertEntry(compareDefine.getExpectValue());
+            log.info("compare key = {} response value={}", pattenEntry.getPropertyKey(),
+                    JSON.toJSONString(compareDefine.getResponseValue()));
             boolean anyMatch = responseList.stream().anyMatch(obj -> {
-                if (Objects.equals(propertyName, SELF)) {
-                    return Objects.equals(compareDefine.getExpectValue(), String.valueOf(obj));
+                if (StringUtils.isBlank(pattenEntry.propertyKey)) {
+                    return Objects.equals(pattenEntry.getExpectValue(), String.valueOf(obj));
                 }
-                Object value = ognlDataParser.exchangeOgnlParamValue(obj, "$body." + propertyName);
-                return Objects.equals(compareDefine.getExpectValue(), String.valueOf(value));
+                Object value = ognlDataParser.exchangeOgnlParamValue(obj, "$body." + pattenEntry.propertyKey);
+                return Objects.equals(pattenEntry.getExpectValue(), String.valueOf(value));
             });
             if (!anyMatch){
                 compareResult.setErrorType(ErrorCode.COMPARE_ERROR);
-                String message = String.format("not find expect value=%s in array", compareDefine.getExpectValue());
+                String message = String.format("not find expect value=%s in array", pattenEntry.getExpectValue());
                 compareResult.setErrorMessage(message);
             }
         }catch (Exception e){
@@ -50,5 +53,24 @@ public class ArrayMatchCompare extends BaseCompare{
             compareResult.setErrorMessage("array match run error");
         }
         return compareResult;
+    }
+
+    public PattenEntry convertEntry(String value) {
+        Matcher matcher = pattern.matcher(value);
+        PattenEntry pattenEntry = new PattenEntry();
+        pattenEntry.setExpectValue(value);
+        if (matcher.find()) {
+            pattenEntry.setPropertyKey(matcher.group(1));
+            pattenEntry.setExpectValue(matcher.group(2));
+        }
+        return pattenEntry;
+    }
+
+    @Data
+    public static class PattenEntry{
+
+        private String propertyKey;
+
+        private String expectValue;
     }
 }
