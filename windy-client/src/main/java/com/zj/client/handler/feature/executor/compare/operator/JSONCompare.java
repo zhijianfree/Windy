3762fc69506613bd055d398041bbd3a828317d1d
guyuelan <1561107002@qq.com>
2025-01-22 17:24:29 +0800

    json比对错误捕获

--- windy-client/src/main/java/com/zj/client/handler/feature/executor/compare/operator/JSONCompare.java
+++ windy-client/src/main/java/com/zj/client/handler/feature/executor/compare/operator/JSONCompare.java
@@ -7,8 +7,12 @@ import com.zj.common.enums.CompareType;
 import com.zj.client.utils.CompareJsonUtils;
 import com.zj.common.exception.ErrorCode;
 import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class JSONCompare extends BaseCompare {
     @Override
@@ -19,9 +23,15 @@ public class JSONCompare extends BaseCompare {
     @Override
     public CompareResult compare(CompareDefine compareDefine) {
         CompareResult compareResult = createSuccessResult();
-        Map<String, Object> result = CompareJsonUtils.compareJsonObject(
-                JSON.toJSONString(compareDefine.getResponseValue()), compareDefine.getExpectValue());
-        if (result.size() != 0){
+        try {
+            Map<String, Object> result = CompareJsonUtils.compareJsonObject(
+                    JSON.toJSONString(compareDefine.getResponseValue()), compareDefine.getExpectValue());
+            if (MapUtils.isNotEmpty(result)){
+                compareResult.setErrorType(ErrorCode.COMPARE_ERROR);
+                compareResult.setErrorMessage("JSON compare error");
+            }
+        }catch (Exception e){
+            log.info("json compare error", e);
             compareResult.setErrorType(ErrorCode.COMPARE_ERROR);
             compareResult.setErrorMessage("JSON compare error");
         }
