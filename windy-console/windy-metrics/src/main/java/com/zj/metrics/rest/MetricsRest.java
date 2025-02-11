816c9ebd2e0e503ff8d887d84808d2da0c701681
guyuelan <1561107002@qq.com>
2025-02-11 18:02:01 +0800

    模块目录调整，以及新增metric指标模块

---
+++ windy-console/windy-metrics/src/main/java/com/zj/metrics/rest/MetricsRest.java
@@ -0,0 +1,9 @@
+package com.zj.metrics.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/devops")
+public class MetricsRest {
+}
---
+++ windy-console/windy-metrics/src/main/java/com/zj/metrics/service/MetricsService.java
@@ -0,0 +1,7 @@
+package com.zj.metrics.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MetricsService {
+}
